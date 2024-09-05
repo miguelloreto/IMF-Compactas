d8f9975d423c1a73ebba867a4c02fee1d62e3c44
MIGUEL DE LORETO NETO <migueldeloreto@gmail.com>
2024-09-05 13:35:11 -0300

    gráficos universalizados

--- ppxf/graphs/todas_galaxias/analise_slope_1.3_salim/analise_amostra_1800.py
+++ ppxf/graphs/todas_galaxias/analise_slope_1.3_salim/analise_amostra_1800.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import scipy 
+import scipy.stats
+plt.rcParams.update({'font.size': 17})
 
+#Note que aqui o slope tem que ser o 1.3 para fazer a comparação com o Salim.
 tabela_massas_calculadas=pd.read_csv("massas_amostra_total.csv")
 tabela_info_MCGs=pd.read_csv("info_MCGs_Mdyn.csv")
 tabela_info_MCGs.drop_duplicates(inplace=True)
@@ -12,7 +14,6 @@ tabela_massas_calculadas["Diferença_Salim"]=tabela_massas_calculadas["Mcor"]-ta
 difmax=tabela_massas_calculadas["Diferença_Salim"].max()
 difmin=tabela_massas_calculadas["Diferença_Salim"].min()
 desvio_padrao_dif_salim=tabela_massas_calculadas["Diferença_Salim"].std()
-print(difmax, difmin, desvio_padrao_dif_salim)
 bins=int((difmax-difmin)/(desvio_padrao_dif_salim*0.5))
 array_mcor=np.array(tabela_massas_calculadas["Mcor"])
 array_salim=np.array(tabela_massas_calculadas["M_Salim"])
@@ -21,22 +22,20 @@ array_dif_salim=np.array(tabela_massas_calculadas['Diferença_Salim'])
 kde=scipy.stats.gaussian_kde(array_dif_salim)
 array_x_kde_plot=np.linspace(difmin, difmax, 1000)
 
-plt.figure(figsize=(6,6))
-plt.hist(array_dif_salim,bins=bins, density=True, color = 'xkcd:lightblue',edgecolor='black')
-plt.plot(array_x_kde_plot, kde(array_x_kde_plot),color='xkcd:blue', label="KDE")
+plt.figure(figsize=(8,8))
+plt.hist(array_dif_salim,bins=bins, density=True, color = 'xkcd:cherry',edgecolor='black', alpha=0.6)
+plt.plot(array_x_kde_plot, kde(array_x_kde_plot),color='xkcd:red', label="KDE")
 plt.legend()
-plt.ylabel("Log de Massa Estelar (MCor)")
-plt.xlabel("Log de Massa Estelar (MCor-MSalim)")
-plt.savefig('histograma_final.jpg', dpi=900)
+plt.ylabel(r"Frequência (normalizada)")
+plt.xlabel(r"$\log(M_\star/M_\odot) - \log(M_{Salim}/M_\odot)$")
+plt.savefig('histograma_final_ch.png', dpi=900)
 
-plt.figure(figsize=(6,6))
-plt.scatter(array_salim, array_mcor, color='xkcd:azure', s=0.5)
+plt.figure(figsize=(8,8))
+plt.scatter(array_salim, array_mcor, color='xkcd:red', s=0.5)
 plt.plot(array_salim, array_salim, color='black')
 plt.plot(array_salim, array_salim-0.5,color='black', linewidth=0.4, label="Dex $\pm 0.5$")
 plt.plot(array_salim, array_salim+0.5,color='black', linewidth=0.4)
 plt.legend()
-plt.ylabel("Log de Massa Estelar (MCor)")
-plt.xlabel("Log de Massa Estelar (MSalim)")
-plt.savefig('plot_comparativo_final.jpg', dpi=900)
-
-
+plt.ylabel(r"$\log(M_\ast/M_\odot)$ (Chabrier)")
+plt.xlabel(r"$\log(M_{Salim}/M_\odot)$")
+plt.savefig('plot_comparativo_final_ch.png', dpi=900)
--- ppxf/graphs/todas_galaxias/fig_lim_sup.py
+++ ppxf/graphs/todas_galaxias/fig_lim_sup.py
@@ -21,7 +21,24 @@ def kde_line(dataframe):
     array_x_kde_plot=np.linspace(difmin, difmax, 1000)
     return array_x_kde_plot, kde(array_x_kde_plot)
 
-n_bins=15
+def get_binned(dados, n_bins,medida):
+  percentiles=np.arange(1/n_bins, 1, 1/n_bins)
+  quantis=[]
+  mediana_medida=[]
+  mediana_gamma=[]
+  erro=[]
+  for i in percentiles: quantis.append(dados[medida].quantile(i))
+  for i in quantis:
+    dados_quantil=dados[dados[medida] < i]
+    mediana_medida.append(np.median(dados_quantil[medida]))
+    mediana_gamma.append(np.median(dados_quantil.lim_sup))
+    erro.append(np.std(dados_quantil.lim_sup))
+    dados_upper=dados[dados[medida]> i]
+    dados=dados_upper
+  return mediana_medida, mediana_gamma, erro
+
+n_bins_hist=15
+n_bins_rel=6
 
 initial_path=os.getcwd()
 mcgs_path=initial_path+'/mcgs/regul'+str(regul)
@@ -29,9 +46,11 @@ csgs_path=initial_path+'/csgs/regul'+str(regul)
 os.chdir(mcgs_path)
 tabela_mcgs_ls=pd.read_csv("tabela_lim_sup.csv")
 tabela_mcgs_ls=tabela_mcgs_ls[tabela_mcgs_ls['lim_sup']  < 5]
+tabela_mcgs_ls=tabela_mcgs_ls[tabela_mcgs_ls['lim_sup']  > 0]
 os.chdir(csgs_path)
 tabela_csgs_ls=pd.read_csv("tabela_lim_sup.csv")
 tabela_csgs_ls=tabela_csgs_ls[tabela_csgs_ls['lim_sup']  < 5]
+tabela_csgs_ls=tabela_csgs_ls[tabela_csgs_ls['lim_sup']  > 0]
 
 ks=scipy.stats.ks_2samp(np.array(tabela_mcgs_ls['lim_sup']), np.array(tabela_csgs_ls['lim_sup']))
 
@@ -45,33 +64,40 @@ r_pearson_sigma_csgs=tabela_csgs_ls["sigma_e"].astype(float).corr(tabela_csgs_ls
 tau_kendall_massa_csgs=tabela_csgs_ls["log_M"].astype(float).corr(tabela_csgs_ls['lim_sup'].astype(float), method='kendall')
 tau_kendall_sigma_csgs=tabela_csgs_ls["sigma_e"].astype(float).corr(tabela_csgs_ls['lim_sup'].astype(float), method='kendall')
 
+mcgs_m=get_binned(tabela_mcgs_ls, n_bins_rel, 'log_M')
+csgs_m=get_binned(tabela_csgs_ls, n_bins_rel, 'log_M')
+mcgs_s=get_binned(tabela_mcgs_ls, n_bins_rel, 'sigma_e')
+csgs_s=get_binned(tabela_csgs_ls, n_bins_rel, 'sigma_e')
+
 os.chdir(initial_path)
 
 plt.figure(figsize=(5,5))
-plt.scatter(np.array(tabela_mcgs_ls["log_M"]), np.array(tabela_mcgs_ls["lim_sup"]), color='xkcd:azure', s=0.7,label=r'MCGs, r={0:.2f}, $\tau={0:.2f}$ '.format(r_pearson_massa_mcgs, tau_kendall_massa_mcgs))
-plt.scatter(np.array(tabela_csgs_ls["log_M"]), np.array(tabela_csgs_ls["lim_sup"]), color='xkcd:red', s=0.7,label=r'CSGs, r={0:.2f}, $\tau={0:.2f}$ '.format(r_pearson_massa_csgs, tau_kendall_massa_csgs))
+plt.scatter(np.array(tabela_mcgs_ls["log_M"]), np.array(tabela_mcgs_ls["lim_sup"]), color='xkcd:cherry', s=0.7,label=r'MCGs, r={0:.2f}, $\tau={0:.2f}$ '.format(r_pearson_massa_mcgs, tau_kendall_massa_mcgs))
+plt.scatter(np.array(tabela_csgs_ls["log_M"]), np.array(tabela_csgs_ls["lim_sup"]), color='xkcd:green', s=0.7,label=r'CSGs, r={0:.2f}, $\tau={0:.2f}$ '.format(r_pearson_massa_csgs, tau_kendall_massa_csgs))
+plt.plot(mcgs_m[0],mcgs_m[1], linestyle='-.', marker='s', color='black')
+plt.plot(csgs_m[0],csgs_m[1], linestyle='-.', marker='o', color='black')
 plt.ylim([0,4])
-plt.title("Limite Superior IMF x $M_{*}$")
-plt.ylabel("Limite Superior IMF")
-plt.xlabel("$\log(M_{*})$ ($M_{\odot}$)")
+plt.ylabel(r"$\Gamma_{max}$")
+plt.xlabel(r"$\log(M_{\star})$ ($M_{\odot}$)")
 plt.legend()
-plt.savefig('limsupxlogm_'+str(regul)+'.jpg', dpi=900)
+plt.savefig('limsupxlogm_'+str(regul)+'.png', dpi=900)
 
 plt.figure(figsize=(5,5))
-plt.scatter(np.log(np.array(tabela_mcgs_ls["sigma_e"])), np.array(tabela_mcgs_ls["lim_sup"]), color='xkcd:azure', s=0.7, label=r'MCGs, r={0:.2f}, $\tau={0:.2f}$'.format(r_pearson_sigma_mcgs, tau_kendall_sigma_mcgs))
-plt.scatter(np.log(np.array(tabela_csgs_ls["sigma_e"])), np.array(tabela_csgs_ls["lim_sup"]), color='xkcd:red', s=0.7, label=r'CSGs, r={0:.2f}, $\tau={0:.2f}$'.format(r_pearson_sigma_csgs, tau_kendall_sigma_csgs))
+plt.scatter(np.log10(np.array(tabela_mcgs_ls["sigma_e"])), np.array(tabela_mcgs_ls["lim_sup"]), color='xkcd:cherry', s=0.7, label=r'MCGs, r={0:.2f}, $\tau={0:.2f}$'.format(r_pearson_sigma_mcgs, tau_kendall_sigma_mcgs))
+plt.scatter(np.log10(np.array(tabela_csgs_ls["sigma_e"])), np.array(tabela_csgs_ls["lim_sup"]), color='xkcd:green', s=0.7, label=r'CSGs, r={0:.2f}, $\tau={0:.2f}$'.format(r_pearson_sigma_csgs, tau_kendall_sigma_csgs))
+plt.plot(np.log10(np.array(mcgs_s[0])), mcgs_s[1], linestyle='-.', marker='s', color='black')
+plt.plot(np.log10(np.array(csgs_s[0])), csgs_s[1], linestyle='-.', marker='o', color='black')
 plt.ylim([0,4])
-plt.title("Limite Superior IMF x $\log(\sigma)$")
-plt.ylabel("Limite Superior IMF")
-plt.xlabel("$\log(\sigma)$ (km/s)")
+plt.ylabel(r"$\Gamma_{max}$")
+plt.xlabel(r"$\log(\sigma_e)$ (km/s)")
 plt.legend()
-plt.savefig('limsupxlogsigma_'+str(regul)+'.jpg', dpi=900)
+plt.savefig('limsupxlogsigma_'+str(regul)+'.png', dpi=900)
 
 plt.rcParams.update({'font.size': 15})
 plt.figure(figsize=(8,8))
-plt.hist(np.array(tabela_mcgs_ls['lim_sup']),bins=n_bins, density=True, color = 'xkcd:cherry',edgecolor='black', label='Distribuição MCGs')
+plt.hist(np.array(tabela_mcgs_ls['lim_sup']),bins=n_bins_hist, density=True, color = 'xkcd:cherry',edgecolor='black', label='Distribuição MCGs')
 plt.axvline(tabela_mcgs_ls['lim_sup'].mean(), color='xkcd:salmon', ls='dashed', label="Média MCGs",linewidth = 3)
-plt.hist(np.array(tabela_csgs_ls['lim_sup']),bins=n_bins, density=True, color = 'xkcd:green',edgecolor='black', label='Distribuição CSGs', alpha=0.7)
+plt.hist(np.array(tabela_csgs_ls['lim_sup']),bins=n_bins_hist, density=True, color = 'xkcd:green',edgecolor='black', label='Distribuição CSGs', alpha=0.7)
 plt.axvline(tabela_csgs_ls['lim_sup'].mean(), color='xkcd:lightgreen', ls='dashed', label="Média CSGs", linewidth = 3)
 plt.plot([], [], ' ', label=r"KS p-value < {0:.0e}".format(1e-10))
 plt.legend()
